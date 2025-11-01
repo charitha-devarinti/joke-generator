9cdce54504387fde7b4079586f663b7f13e6acd8
charitha-devarinti <charitha.devarinti@gmail.com>
2025-11-01 21:06:53 +0530

    intial commit

---
+++ joke_genertor.js
@@ -0,0 +1,24 @@
+const btn=document.querySelector('.btn');
+const jokEle=document.querySelector('.joke');
+
+function generateJoke(){
+    const xhr=new XMLHttpRequest();
+    xhr.open('GET','https://api.chucknorris.io/jokes/random');
+    xhr.onreadystatechange=function(){
+        if(this.readyState===4 && this.status===200){
+            const joke=JSON.parse(this.responseText).value;
+             jokEle.innerHTML=joke;
+    }
+ 
+}
+     xhr.send();
+}
+
+
+
+
+
+
+
+btn.addEventListener('click',generateJoke);
+document.addEventListener('DOMContentLoaded',generateJoke);
